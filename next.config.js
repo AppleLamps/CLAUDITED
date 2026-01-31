cf1cb426ede9f409e3e3d56c140d0f942b9008bd
AppleLamps <applelampsx2024@gmail.com>
2026-01-31 04:41:45 -0500

    refactor: format next.config.js for consistency

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
-CLAUDITED\next.config.js
-```javascript
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    ppr: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+    experimental: {
+        ppr: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
